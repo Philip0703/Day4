2a6977750a95173ed559ada644702da1c03cfb5e
Philip M Shajan <76397841+Philip0703@users.noreply.github.com>
2023-12-15 15:13:11 +0530

    Revert "added"
    This reverts commit 95b964a7be8bcddb2a1bc79342e8847b563bf6f2.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { DistrictsComponent } from './pages/districts/districts.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { SingleComponent } from './pages/single/single.component';
 
-export const routes: Routes = [
-    {
-        path:'',component:HomeComponent
-    },
-    
-    {
-        path:'contact',component:ContactComponent
-    },
-    {
-        path:'districts',component:DistrictsComponent
-    },
-    {
-        path:'signup',component:SignupComponent
-    },
-    {
-        path:'districts/:id',component:SingleComponent
-    }
-];
+export const routes: Routes = [];
