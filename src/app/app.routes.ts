1e09a9ccfdaec22b4d87fc02f3b813c41ee13f22
Philip M Shajan <76397841+Philip0703@users.noreply.github.com>
2023-12-16 15:50:53 +0530

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { DistrictsComponent } from './pages/districts/districts.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SingleComponent } from './pages/single/single.component';
+
+export const routes: Routes = [
+    {
+        path:'',component:HomeComponent
+    },
+    
+    {
+        path:'contact',component:ContactComponent
+    },
+    {
+        path:'districts',component:DistrictsComponent
+    },
+    {
+        path:'signup',component:SignupComponent
+    },
+    {
+        path:'districts/:id',component:SingleComponent
+    }
+];
